74a218c93ffd6af7e46c738ee8ead1e476be5c8c
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-01-17 16:40:54 +0530

    Create POSITIVE.C
    to find given number is positive or negative or zero

---
+++ POSITIVE.C
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the number");
+scanf("%d",&a);
+if(a>0)
+{
+printf("the given number is positive");
+}
+if(a<0)
+{
+printf("the given number is negative");
+}
+if(a==0)
+{
+printf("the given number is zero");
+}
+return 0;
+}
